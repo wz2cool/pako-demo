6cb8152389eaa689aa862d3489073c7271c47c87
wz2cool <wz2cool@live.cn>
2019-02-02 14:17:05 +0800

    add

---
+++ java/src/main/java/com/github/wz2cool/demo/pakojs/PakojsApplication.java
@@ -0,0 +1,14 @@
+package com.github.wz2cool.demo.pakojs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakojsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PakojsApplication.class, args);
+	}
+
+}
+
